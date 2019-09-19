63a34083e749b0afb4edd2c7fb066418b309a4f4
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 18:07:11 +0530

    Ascii Art Using For Loop

---
+++ src/com/aditya/java/ForLoopStars.java
@@ -0,0 +1,28 @@
+package com.aditya.java;
+
+public class ForLoopStars {
+	public static void main(String[] args) {
+		int row, numberOfStars;
+		
+		for(;;) {
+		for (row = 1; row <= 100; row++) {
+			for (numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
+				System.out.print("*");
+			}
+
+			// Go To Next Line
+			System.out.println();
+		}
+		for (row = 0; row <= 100 ; row++) {
+			for (numberOfStars = 100 ; numberOfStars >=  row; numberOfStars--) {
+				System.out.print("*");
+			}
+
+			// Go To Next Line
+			System.out.println();
+		}
+		
+		}
+
+	}
+}
