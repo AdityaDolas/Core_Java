0d9fa3a0c799022baa23117988619c9a20f5e260
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 18:22:04 +0530

    MultiplicationTable

---
+++ src/com/aditya/java/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package com.aditya.java;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		int n, c;
+
+		System.out.println("Enter an integer to print it's multiplication table:");
+
+		Scanner in = new Scanner(System.in);
+
+		n = in.nextInt();
+		System.out.println("Table of " + n+" :");
+
+		for (c = 1; c <= 10; c++) {
+			System.out.println(n + "*" + c + " = " + (n * c));
+		}
+	}
+}
