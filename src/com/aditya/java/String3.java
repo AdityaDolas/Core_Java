ea1cea9c972235523508b7fa3e6a9e5dd82a7197
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 22:47:30 +0530

    String3

---
+++ src/com/aditya/java/String3.java
@@ -0,0 +1,14 @@
+package com.aditya.java;
+
+public class String3 {
+
+	public static void main(String[] args) {
+		String s="hell";
+		String s1="Hello";
+		String s2="Hello";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s.equals(s1));
+	}
+
+}
