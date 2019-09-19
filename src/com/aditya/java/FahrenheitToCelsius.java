e59a39ce52cd37cb5a282f604ba7a4fa882a91ee
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 18:33:26 +0530

    FahrenheitToCelsius

---
+++ src/com/aditya/java/FahrenheitToCelsius.java
@@ -0,0 +1,14 @@
+package com.aditya.java;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+	public static void main(String[] args) {
+		System.out.println("Enter Integer:");
+		Scanner in=new Scanner(System.in);
+		float temp=in.nextInt();
+		
+		temp=((temp - 32)*5)/9;
+		System.out.println("temperature in Celsius = " + temp);
+	}
+}
