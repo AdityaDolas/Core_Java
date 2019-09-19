6901ea28e3406a844f367bbcc0556657da4a2d97
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 22:31:06 +0530

    Method2

---
+++ src/com/aditya/java/Method2.java
@@ -0,0 +1,32 @@
+package com.aditya.java;
+
+import java.util.Scanner;
+
+public class Method2 {
+	void swap(int num1, int num2) {
+		System.out.println("Before"+num1+"&"+num2);
+		
+		int temp;
+		
+		temp=num1;
+		num1=num2;
+		num2=temp;
+		
+		System.out.println("After"+num1+"&"+num2);
+	}
+	public static void main(String[] args) {
+		int n,m;
+		
+		System.out.println("Enter the value of m");
+		Scanner scanner=new Scanner(System.in);
+		
+		m=scanner.nextInt();
+		System.out.println("Enter the value of m");
+		
+		n=scanner.nextInt();
+		
+		Method2 mt=new Method2();
+		mt.swap(m, n);
+		
+	}
+}
