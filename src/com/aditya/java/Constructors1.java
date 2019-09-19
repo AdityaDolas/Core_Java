1eae35eaa59cf6738f5f550fbabffe3597da3498
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 22:34:33 +0530

    Constructor1

---
+++ src/com/aditya/java/Constructors1.java
@@ -0,0 +1,18 @@
+package com.aditya.java;
+
+public class Constructors1 {
+	public Constructors1() {
+		System.out.println("Default Constructor");
+		
+	}
+	
+	public Constructors1(int m) {
+		System.out.println("Parameterized Constructor");
+		
+	}
+	
+	public static void main(String[] args) {
+		Constructors1 c1=new Constructors1();
+		Constructors1 c2=new Constructors1(8);
+	}
+}
