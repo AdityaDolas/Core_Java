c876e67e3a68fc13f9befb1339dbecdab55245d0
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 17:03:32 +0530

    IfElse

---
+++ src/com/aditya/java/Ifelse.java
@@ -0,0 +1,12 @@
+package com.aditya.java;
+
+public class Ifelse {
+	public static void main(String[] args) {
+		boolean learning=true;
+		if(learning) {
+			System.out.println("good");
+		}else{
+			System.out.println("not good");
+		}
+	}
+}
