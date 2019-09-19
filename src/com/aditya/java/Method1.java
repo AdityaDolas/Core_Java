c2675bb5562f2e131aa458dad260b94539f05027
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 22:25:02 +0530

    Method1

---
+++ src/com/aditya/java/Method1.java
@@ -0,0 +1,13 @@
+package com.aditya.java;
+
+public class Method1 {
+	int a;
+	
+	void counter(){
+		System.out.println(a);
+	}
+	public static void main(String[] args) {
+		Method1 method1=new Method1();
+		method1.counter();
+	}
+}
