75fd54d99cd721bc6ab8d8428abe207510b7b4ac
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 22:46:24 +0530

    String2

---
+++ src/com/aditya/java/String2.java
@@ -0,0 +1,14 @@
+package com.aditya.java;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = "java";
+		String s3 = new String("java");
+
+		System.out.println(s1 == s2); // true
+		System.out.println(s1 == s3); // false
+	}
+
+}
