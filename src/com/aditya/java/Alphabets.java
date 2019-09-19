c0cd6e30c8ad4a1e2e272ad271aa4f0fba44281f
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 18:16:30 +0530

    Alphabets

---
+++ src/com/aditya/java/Alphabets.java
@@ -0,0 +1,11 @@
+package com.aditya.java;
+
+public class Alphabets {
+	public static void main(String[] args) {
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++) {
+			System.out.print(" "+c);
+		}
+	}
+}
