6d5f83473a1d6eda9a05a9d73ff0220620cc707f
Aditya Dolas <dolasadi@gmail.com>
2019-09-19 22:48:12 +0530

    String4

---
+++ src/com/aditya/java/String4.java
@@ -0,0 +1,13 @@
+package com.aditya.java;
+
+public class String4 {
+
+	public static void main(String[] args) {
+		String s = "Hello";
+		String str = "Folks";
+		String str2 = s + str;
+		System.out.println("Hello" + "Floks");
+
+	}
+
+}
