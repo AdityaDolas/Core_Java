7af6ab0291dfbfb077eae60210c0e7a5e2d11c4a
Aditya Dolas <dolasadi@gmail.com>
2019-09-23 23:58:50 +0530

    aa

--- src/com/aditya/java/Tree.java
+++ src/com/aditya/java/Tree.java
@@ -1,16 +1,20 @@
 package com.aditya.java;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Tree {
 	public static void main(String[] args) {
-		TreeSet<String> t = new TreeSet<>();
+		TreeSet<Integer> t = new TreeSet<>();
+		HashSet<Integer> h = new HashSet<>();
 		for (;;) {
 			System.out.println("Enter:");
 			Scanner sc = new Scanner(System.in);
-			String s = sc.next();
+			int s = sc.nextInt();
 			t.add(s);
+			h.add(t+2);
 			System.out.println(t);
 		}
 	}
---
+++ src/com/aditya/java/aa.java
@@ -0,0 +1,7 @@
+package com.aditya.java;
+
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
