2c72f77e776e920aa79fd3441783cb4a5458a4b2
Aditya Dolas <dolasadi@gmail.com>
2019-09-23 19:27:31 +0530

    HashSet

---
+++ src/com/aditya/java/HashSet_Contacts.java
@@ -0,0 +1,38 @@
+package com.aditya.java;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class HashSet_Contacts {
+	public static void main(String[] args) {
+		HashSet<String> h = new HashSet<>();
+
+		// Asking for input
+		for (;;) {
+			System.out.println("Enter : ");
+			Scanner sc = new Scanner(System.in);
+			String s = sc.next();
+
+			// Adding elements using HashSet
+			h.add(s);
+			
+			// Displaying the HasSet
+			System.out.println(h);
+			
+			// Iterating over HasSet items
+			System.out.println("List:");
+			Iterator<String> i=h.iterator();
+			while (i.hasNext()) {
+				System.out.println(i.next());
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
