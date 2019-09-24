3d177461b2be07de098e78301a04eb07b8f32788
Aditya Dolas <dolasadi@gmail.com>
2019-09-24 19:16:54 +0530

    Factorial Tree

--- src/com/aditya/java/Factorial.java
+++ src/com/aditya/java/Factorial.java
@@ -1,19 +1,24 @@
 package com.aditya.java;
 
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Factorial {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int i;
-		int f = 1;
+		TreeSet<Integer> t=new TreeSet<>();
 
-		for (i = 1; i <=num; i++) {
-			f = f * i;
+		for (;;) {
+			System.out.println("Enter Number : ");
+			Scanner sc = new Scanner(System.in);
+			int num = sc.nextInt();
+			int f = 1;
 
+			for (int i = 1; i <= num; i++) {
+				f = f * i;
+			}
+			System.out.println(f);
+			t.add(f);
+			System.out.println(t);
 		}
-		System.out.println(f);
-
 	}
 }
