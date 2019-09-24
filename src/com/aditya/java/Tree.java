d4cf2d533ddae7b433b02262dc06a0dddae324d3
Aditya Dolas <dolasadi@gmail.com>
2019-09-24 14:49:13 +0530

    TreeSet2

--- src/com/aditya/java/Tree.java
+++ src/com/aditya/java/Tree.java
@@ -1,21 +1,21 @@
 package com.aditya.java;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.TreeSet;
 
 public class Tree {
 	public static void main(String[] args) {
+
 		TreeSet<Integer> t = new TreeSet<>();
-		HashSet<Integer> h = new HashSet<>();
+		TreeSet<Integer> h = new TreeSet<>();
 		for (;;) {
 			System.out.println("Enter:");
 			Scanner sc = new Scanner(System.in);
 			int s = sc.nextInt();
 			t.add(s);
-			h.add(t+2);
+			h.add(s * s);
 			System.out.println(t);
+			System.out.println(h);
 		}
 	}
 }
