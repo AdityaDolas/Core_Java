46f7a8b91d310704f36a5228c61f27baac1aeaf4
Aditya Dolas <dolasadi@gmail.com>
2019-09-25 23:50:45 +0530

    BufferReader1

---
+++ src/com/aditya/java/BufferReader1.java
@@ -0,0 +1,15 @@
+package com.aditya.java;
+
+import java.util.Scanner;
+
+public class BufferReader1 {
+	public static void main(String args[]) throws Exception {
+
+		Scanner s = new Scanner(System.in);
+		int i = s.nextInt();
+		String st =s.next();
+		System.out.println(i * 2);
+		System.out.println(st);
+	}
+
+}
