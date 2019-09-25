a113a16f516f80c48e6c589c850c85ee64c353e3
Aditya Dolas <dolasadi@gmail.com>
2019-09-25 23:57:13 +0530

    BufferReader2

--- src/com/aditya/java/BufferReader1.java
+++ src/com/aditya/java/BufferReader1.java
@@ -1,15 +1,21 @@
 package com.aditya.java;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class BufferReader1 {
 	public static void main(String args[]) throws Exception {
 
-		Scanner s = new Scanner(System.in);
-		int i = s.nextInt();
-		String st =s.next();
-		System.out.println(i * 2);
-		System.out.println(st);
+		/*
+		 * Scanner s = new Scanner(System.in); int i = s.nextInt(); String st =s.next();
+		 * System.out.println(i * 2); System.out.println(st);
+		 */
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a=Integer.parseInt(br.readLine());
+		String b=br.readLine();
+		System.out.println(a*2);
+		System.out.println(b);
 	}
 
 }
