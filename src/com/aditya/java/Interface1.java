751c1768fe8ef43c51f31024f416ca586cc25909
Aditya Dolas <dolasadi@gmail.com>
2019-09-25 22:11:57 +0530

    Interface

---
+++ src/com/aditya/java/Interface1.java
@@ -0,0 +1,24 @@
+package com.aditya.java;
+
+interface Drawable{
+	void draw();
+}
+
+class Rectangle2 implements Drawable {
+	public void draw() {
+		System.out.println("drawing rectangle");
+	}
+}
+
+class Circle2 implements Drawable{
+	public void draw() {
+		System.out.println("drawing circle");
+	}
+}
+
+public class Interface1 {
+	public static void main(String[] args) {
+		Drawable d=new Circle2();
+		d.draw();
+	}
+}
