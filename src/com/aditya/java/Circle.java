46c4463d9ff691c68ed72a1508e2d7dd44ef27e6
Aditya Dolas <dolasadi@gmail.com>
2019-09-25 21:23:31 +0530

    Abstract2

---
+++ src/com/aditya/java/Circle.java
@@ -0,0 +1,22 @@
+package com.aditya.java;
+
+abstract class Shape{
+	abstract void draw();
+}
+
+class Rectangle extends Shape {
+	void draw() {
+		System.out.println("drawing rectangle");
+	}
+}
+
+public class Circle extends Shape{
+	void draw() {
+		System.out.println("drawing Circle");
+		
+	}
+	public static void main(String[] args) {
+		Shape s=new Circle();
+		s.draw();
+	}
+}
