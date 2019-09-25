5f784a508a25bf84fa7b8fbdcca51255f069ed7e
Aditya Dolas <dolasadi@gmail.com>
2019-09-25 21:55:51 +0530

    Abstract3

---
+++ src/com/aditya/java/TestBank.java
@@ -0,0 +1,24 @@
+package com.aditya.java;
+
+abstract class Bank{
+	abstract int getRateOffInterast();
+}
+
+class SBI extends Bank{
+	int getRateOffInterast() {
+		return 7;
+	}
+}
+
+class PNB extends Bank{
+	int getRateOffInterast() {
+		return 8;
+	}
+}
+
+public class TestBank {
+	public static void main(String[] args) {
+		Bank b=new SBI();
+		System.out.println("Rate Of Interast");
+	}
+}
