b7af80b1eb90f94ab7c62fcbf3d9c73335083ba0
Aditya Dolas <dolasadi@gmail.com>
2019-09-26 21:00:31 +0530

    LinkedList1

---
+++ src/com/aditya/java/HashMap1.java
@@ -0,0 +1,19 @@
+package com.aditya.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMap1 {
+	public static void main(String[] args) {
+		HashMap<Integer, String> hm=new HashMap<>();
+		System.out.println(hm);
+		
+		hm.put(100, "avinash");
+		
+		System.out.println("after invoking");
+		for (Map.Entry m:hm.entrySet()){
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+		
+	}
+}
