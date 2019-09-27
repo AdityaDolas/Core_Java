edab4e9eb4d03ebc41bfde2bcc61f20c25b7a795
Aditya Dolas <dolasadi@gmail.com>
2019-09-27 21:45:34 +0530

    File_notFound

---
+++ src/com/aditya/java/File_notFound.java
@@ -0,0 +1,17 @@
+package com.aditya.java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class File_notFound {
+	public static void main(String[] args) {
+		try {
+			File file=new File("E://file.txt");
+			
+			FileReader fr=new FileReader(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("File Does Not Exist.");
+		}
+	}
+}
