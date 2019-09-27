f5be8d2ae043534f8a539baa1e41c41adf3762e5
Aditya Dolas <dolasadi@gmail.com>
2019-09-27 21:20:37 +0530

    Locale2

---
+++ src/com/aditya/java/Locale2.java
@@ -0,0 +1,13 @@
+package com.aditya.java;
+
+import java.util.Locale;
+
+public class Locale2 {
+	public static void main(String[] args) {
+		Locale enLocale=new Locale("en", "US");
+		Locale frLocale=new Locale("fr", "FR");
+		Locale esLocale=new Locale("es", "ES");
+		System.out.println("English : " +enLocale.getDisplayCountry());
+		System.out.println("French : "+frLocale.getDisplayLanguage());
+	}
+}
