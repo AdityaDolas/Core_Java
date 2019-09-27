e4df26d687cca4a02db2b5331b7929cac8ab9c94
Aditya Dolas <dolasadi@gmail.com>
2019-09-27 21:49:40 +0530

    ArrayIndexOutOfBoundExcep

---
+++ src/com/aditya/java/ArrayIndexOutOfBoundExcep.java
@@ -0,0 +1,12 @@
+package com.aditya.java;
+
+public class ArrayIndexOutOfBoundExcep {
+	public static void main(String args[]) {
+		try {
+			int a[] = new int[5];
+			a[6] = 9;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array Index is Out Of Bounds");
+		}
+	}
+}
