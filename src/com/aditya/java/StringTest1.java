ac574bdfbef6e8e560dd523d0a2c64b261cee4cb
Aditya Dolas <dolasadi@gmail.com>
2019-09-29 00:11:48 +0530

    String Test7

--- src/com/aditya/java/StringTest1.java
+++ src/com/aditya/java/StringTest1.java
@@ -1,11 +1,6 @@
 package com.aditya.java;
 
-//Java code to illustrate String 
-
-import java.io.*;
-import java.lang.*;
-
- public class StringTest1 {
+public class StringTest1 {
 	public static void main(String[] args) {
 		// Declare String without using new operator
 		String s = "GeeksforGeeks";
@@ -15,18 +10,22 @@ import java.lang.*;
 
 		// Declare String using new operator
 		String s1 = new String("GeeksforGeeks");
-		
-		String s1 = new String("GeeksforGeeks");
-		
-		String s1 = new String("GeeksforGeeks");
-		
-		String s1 = new String("GeeksforGeeks");
-		
-		String s1 = new String("GeeksforGeeks");
-		
-		String s1 = new String("GeeksforGeeks");
+
+		String s2 = new String("GeeksforGeekss");
+
+		String s3 = new String("GeeksforGeeksss");
+
+		String s4 = new String("GeeksforGeekssss");
+
+		String s5 = new String("GeeksforGeekssssss");
+
+		String s6 = new String("GeeksforGeeksssssss");
 
 		// Prints the String.
 		System.out.println("String s1 = " + s1);
+		System.out.println("String s2 = " + s2);
+		System.out.println("String s3 = " + s3);
+		System.out.println("String s4 = " + s4);
+		System.out.println("String s5 = " + s5);
 	}
 }
