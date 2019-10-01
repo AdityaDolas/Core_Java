2f618ded70c98ba3c0a0668d026799918e637046
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 15:56:29 +0530

    Enum1

---
+++ src/com/aditya/java/Enum1.java
@@ -0,0 +1,15 @@
+package com.aditya.java;
+
+enum Mango {
+	Charie(10), Fairchild(20), haden(80);
+	private int price;
+
+	Mango(int p) {
+		price = p;
+	}int getPrice(){return price;}}
+class Enum1{
+	public static void main(String[] args) {
+		Mango p=Mango.Charie;
+		System.out.println(p.getPrice());
+	}
+}
