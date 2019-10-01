c2db0bc6cd05a7befdbc81e86a580ff2af7d5999
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 17:22:46 +0530

    StringBuilder()

---
+++ src/com/aditya/java/StringBuilder_demo.java
@@ -0,0 +1,16 @@
+package com.aditya.java;
+
+public class StringBuilder_demo {
+	void sbMethod() {
+		StringBuilder sb=new StringBuilder("fruits ");
+		sb.append("are good for health.");
+		System.out.println(sb);
+		System.out.println(sb.delete(6, 10));
+		sb.insert(6, " are ");
+		System.out.println(sb.toString());
+	}
+	public static void main(String[] args) {
+		StringBuilder_demo sb=new StringBuilder_demo();
+		sb.sbMethod();
+	}
+}
