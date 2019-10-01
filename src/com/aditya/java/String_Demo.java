cd97e3d647170737e6c33ea11a2c7ea03507531e
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 17:00:27 +0530

    indexOf()

--- src/com/aditya/java/String_Demo.java
+++ src/com/aditya/java/String_Demo.java
@@ -12,6 +12,7 @@ public class String_Demo {
 		System.out.println(s2.equals(s1));
 		System.out.println(s1.compareTo(s2));
 		System.out.println(s1.startsWith("H"));
+		System.out.println(s2.indexOf("o"));
 		
 	}
 	public static void main(String[] args) {
