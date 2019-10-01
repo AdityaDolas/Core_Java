67971fc653195bebc8a5e5fc25bf936b7b5f2e40
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 15:35:57 +0530

    Array for each

--- src/com/aditya/java/Array1.java
+++ src/com/aditya/java/Array1.java
@@ -3,8 +3,8 @@ package com.aditya.java;
 public class Array1 {
 	public static void main(String[] args) {
 		String j[] = { "aditya", "avinash", "prashant" };
-		for (int i = 0; i <= j.length; i++) {
-			System.out.println(j[i]);
+		for (String i : j) {
+			System.out.println(i);
 		}
 
 	}
