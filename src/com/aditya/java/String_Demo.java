8f54b69d2ccbcb062b55d6255478c52c4f70c944
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 16:25:50 +0530

    String_Demo charAt()

--- src/com/aditya/java/String_Demo.java
+++ src/com/aditya/java/String_Demo.java
@@ -9,6 +9,8 @@ public class String_Demo {
 	
 		System.out.println(s1.length());
 		System.out.println(s1.charAt(1));
+		System.out.println(s2.equals(s1));
+		System.out.println(s1.compareTo(s2));
 		
 	}
 	public static void main(String[] args) {
