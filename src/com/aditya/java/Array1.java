aa6a1e7de5c825a22447c7253653d780c6e58c4a
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 15:33:09 +0530

    Array2

--- src/com/aditya/java/Array1.java
+++ src/com/aditya/java/Array1.java
@@ -3,6 +3,9 @@ package com.aditya.java;
 public class Array1 {
 	public static void main(String[] args) {
 		String j[] = { "aditya", "avinash", "prashant" };
-		System.out.println(j[1]);
+		for (int i = 0; i <= j.length; i++) {
+			System.out.println(j[i]);
+		}
+
 	}
 }
