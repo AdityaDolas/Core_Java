da436fdebcca60e93f841203df7f9c4030854e2d
Aditya Dolas <dolasadi@gmail.com>
2019-10-01 15:45:09 +0530

    TD Array for each

--- src/com/aditya/java/Array1.java
+++ src/com/aditya/java/Array1.java
@@ -1,11 +1,27 @@
 package com.aditya.java;
 
 public class Array1 {
-	public static void main(String[] args) {
+
+	void array1() {
 		String j[] = { "aditya", "avinash", "prashant" };
 		for (String i : j) {
 			System.out.println(i);
 		}
+	}
 
+	void array2() {
+		String[][] j = { { "aditya", "avinash" }, { "avinash", "avinash" }, { "prashant", "prashant" } };
+		for (String[] s : j) {
+			for (String i : s) {
+				System.out.println(i);
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Array1 a1 = new Array1();
+		a1.array1();
+		a1.array2();
 	}
 }
