b15d0a35d84fcb297d24f9462947be8931de785c
Aditya Dolas <dolasadi@gmail.com>
2019-10-03 19:52:45 +0530

    HashTable1

---
+++ src/com/aditya/java/HashTable1.java
@@ -0,0 +1,18 @@
+package com.aditya.java;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class HashTable1 {
+	public static void main(String[] args) {
+		Hashtable<Integer, String> ht = new Hashtable<>();
+		for (;;) {
+			System.out.println("Enter:");
+			Scanner sc = new Scanner(System.in);
+			Integer i = sc.nextInt();
+			String s = sc.next();
+			ht.put(i, s);
+			System.out.println(ht);
+		}
+	}
+}
