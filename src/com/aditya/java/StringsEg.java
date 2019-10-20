4945549699b32cc108484f2cf93aa9a494b08c82
Aditya Dolas <dolasadi@gmail.com>
2019-10-21 03:17:39 +0530

    lastIndexOf()

--- src/com/aditya/java/StringsEg.java
+++ src/com/aditya/java/StringsEg.java
@@ -2,7 +2,9 @@ package com.aditya.java;
 
 public class StringsEg {
 
-	// How to compare two Strings
+	// ===> How to compare two Strings ?
+
+	// String Compare by compareTo()
 
 	protected void compairTo() {
 		String str = "Hello World";
@@ -25,23 +27,37 @@ public class StringsEg {
 		System.out.println(s1.equals(s2));
 		System.out.println(s2.equals(s3));
 	}
-	
+
 	// String Compare by equalsTo()
-	
+
 	protected void equalsTo() {
 		String s1 = "aditya";
 		String s2 = "aditya";
-		String s3 = new String("aditya"); 
+		String s3 = new String("aditya");
+
+		System.out.println(s1 == s2);
+		System.out.println(s2 == s3);
+	}
+	
+	// ===>How to search the last position of a substring ?
+	
+	private void lastIndex() {
+		String s1 = "Hello world ,Hello Reader";
+		int lastIndexOf = s1.lastIndexOf("Hello");
 		
-		System.out.println(s1==s2);
-		System.out.println(s2==s3);
+		if (lastIndexOf == -1) {
+			System.out.println("Hello Not Found");
+		} else {
+			System.out.println("The index is : "+lastIndexOf);
+		}
 	}
 
 	public static void main(String[] args) {
 		StringsEg st = new StringsEg();
 		// st.compairTo();
 		// st.equals1();
-		//st.equalsTo();
+		// st.equalsTo();
+		st.lastIndex();
 
 	}
 }
