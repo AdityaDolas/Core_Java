9641878081dd3bc8cbcb10a7f06c8434206f0fbd
Aditya Dolas <dolasadi@gmail.com>
2019-10-21 03:02:41 +0530

    Comparing Strings equals()

--- src/com/aditya/java/StringsEg.java
+++ src/com/aditya/java/StringsEg.java
@@ -1,14 +1,35 @@
 package com.aditya.java;
 
-//How to compare two Strings
 public class StringsEg {
-	public static void main(String[] args) {
+
+	// How to compare two Strings
+
+	protected void compairTo() {
 		String str = "Hello World";
 		String str2 = "hello world";
 		Object objstr = str;
-		
+
 		System.out.println(str.compareTo(str2));
 		System.out.println(str.compareToIgnoreCase(str2));
 		System.out.println(str.compareTo(objstr.toString()));
+
+	}
+
+	// String Compare by equals()
+
+	protected void equalsTo() {
+		String s1 = "aditya";
+		String s2 = "aditya";
+		String s3 = new String("adi tya");
+
+		System.out.println(s1.equals(s2));
+		System.out.println(s2.equals(s3));
+	}
+
+	public static void main(String[] args) {
+		StringsEg st1 = new StringsEg();
+		// st1.compairTo();
+		// st1.equalsTo();
+
 	}
 }
