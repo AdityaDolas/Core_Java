ec55d5cd70dc03b9fd21bba769ebf625cbbe8167
Aditya Dolas <dolasadi@gmail.com>
2019-10-21 21:39:39 +0530

    stringOptimazedMethod()

--- src/com/aditya/java/StringsEg.java
+++ src/com/aditya/java/StringsEg.java
@@ -198,6 +198,45 @@ public class StringsEg {
 		System.out.println(
 				"Time taken for creation" + " of String objects : " + (endTime1 - startTime1) + " milli seconds");
 	}
+	
+	public void stringOptimazedMethod() {
+		{
+		      String variables[] = new String[50000];	  
+		      for( int i = 0; i < 50000; i++) {
+		         variables[i] = "s"+i;
+		      }
+		      long startTime0 = System.currentTimeMillis();
+		      
+		      for(int i = 0; i < 50000; i++) {
+		         variables[i] = "hello";
+		      }
+		      long endTime0 = System.currentTimeMillis();
+		      System.out.println("Creation time" 
+		         + " of String literals : "+ (endTime0 - startTime0) 
+		         + " ms" );
+		      long startTime1 = System.currentTimeMillis();
+		      
+		      for(int i = 0; i < 50000; i++) {
+		         variables[i] = new String("hello");
+		      }
+		      long endTime1 = System.currentTimeMillis();
+		      System.out.println("Creation time of" 
+		         + " String objects with 'new' key word : " 
+		         + (endTime1 - startTime1)
+		         + " ms");
+		      long startTime2 = System.currentTimeMillis();
+		      
+		      for(int i = 0; i < 50000; i++) {
+		         variables[i] = new String("hello");
+		         variables[i] = variables[i].intern();		  
+		      }
+		      long endTime2 = System.currentTimeMillis();
+		      System.out.println("Creation time of" 
+		         + " String objects with intern(): " 
+		         + (endTime2 - startTime2)
+		         + " ms");
+		   }
+	}
 
 	public static void main(String[] args) {
 		StringsEg st = new StringsEg();
@@ -215,7 +254,8 @@ public class StringsEg {
 		// st.splitString();
 		// st.upperCase();
 		// st.rigion();
-		st.stringComparePerformancr();
+		// st.stringComparePerformancr();
+		st.stringOptimazedMethod();
 
 	}
 }
