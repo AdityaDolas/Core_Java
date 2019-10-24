a3a55e79648d5fabd8f54439b3fe2aaa31ebb49e
Aditya Dolas <dolasadi@gmail.com>
2019-10-24 19:36:29 +0530

    final keyword

--- src/com/aditya/java/Oops.java
+++ src/com/aditya/java/Oops.java
@@ -1,6 +1,8 @@
 package com.aditya.java;
 
 public class Oops {
+	
+	// Private method hello
 	private void hello() {
 		String s = "Hello, World !";
 		System.out.println(s);
@@ -10,6 +12,22 @@ public class Oops {
 		// Can't use the following, because variable s is declared in private method
 		// System.out.println(s);
 	}
+	
+	// final keyword is applicable to method variable and classes.
+	// final keyword cannot be used with an interface in Java.
+	// final keyword used with data members means value cannot be replaced or modify.
+	// Example :-
+	
+	public void finalExample() {
+		final int i = 45;
+		// following line will give compile time error.
+		// i = 45;
+	}
+	
+	// Class that is declared as final cannot be used for the purpose of Inheritance.
+	// Method declared as final cannot be Overridden.
+	
+	
 	public static void main(String[] args) {
 		Oops o = new Oops();
 		o.hello();
