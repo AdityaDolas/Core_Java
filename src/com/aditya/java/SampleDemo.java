0f52158ec8bf3c92ad6bc658f61bc0e4c8840c3c
Aditya Dolas <dolasadi@gmail.com>
2019-10-25 17:58:30 +0530

    String Methods

--- src/com/aditya/java/SampleDemo.java
+++ src/com/aditya/java/SampleDemo.java
@@ -91,15 +91,88 @@ public class SampleDemo {
 		System.out.println("Parameterized Constructor");
 	}
 	
+	public void StringDemo() {
+		String String1 = "Aditya Dolas ";
+		String String2 = "aditya dolas";
+		
+		System.out.println("String1: "+String1);
+		System.out.println("String2: "+String2);
+		
+		// length()
+		System.out.println("String1 length: "+String1.length());
+		
+		// charAt(index)
+		System.out.println("String1 character at index 3: "+String1.charAt(3));
+		
+		// getChars()
+		char[] sa = new char[3];
+		String1.getChars(2, 4, sa, 0);
+		System.out.println("Value Coppied : "+sa);
+		
+		// equals()
+		boolean reasult = String1.equals(String2);
+		System.out.println("String1 equals String2: "+reasult);
+		
+		// compareTo()
+		System.out.println("String1 compareTo String2: "+String1.compareTo(String2));
+		
+		// startsWith()
+		System.out.println("String1 Starts with A: "+String1.startsWith("A"));
+		
+		// endsWith()
+		System.out.println("String1 Ends with s: "+String1.endsWith("s"));
+		
+		// indexOf()
+		System.out.println("String1 index Of s: "+String1.indexOf('s'));
+		
+		// lastIndexof()
+		System.out.println("String1 last Index Of a: "+String1.lastIndexOf('s'));
+		
+		// subString()
+		System.out.println("String1 substring after index of 5: "+String1.substring(5));
+		
+		// concat()
+		System.out.println("String1 concat String2: "+String1.concat(String2));
+		
+		// replace()
+		System.out.println("String1 replaced character a to z: "+String1.replace('a', 'z'));
+		
+		// toUpperCase()
+		System.out.println("String1 toUpperCase: "+String1.toUpperCase());
+		
+		// toLowerCase()
+		System.out.println("String1 toLowerCase : "+String1.toLowerCase());
+		
+		// trim()
+		System.out.println("String1 trim: "+String1.trim());
+		
+		// toChatArray
+		System.out.print("Value Returned: ");
+		char[] ch = String1.toCharArray();
+		System.out.println("String1 toCharArray: "+ch);
+		
+		// valueOf()
+		int a = 50;
+		double d = 56.45;
+		System.out.println("Value of int a: "+a);
+		System.out.println("Value of double d: "+d);
+		char[] arr = {'a', 'd'};
+		System.out.println("a: "+String.valueOf(a));
+		System.out.println("d: "+String.valueOf(d));
+		
+		// equalsIgnoreCase()
+		System.out.println("String1 equalsIgnoreCase String2: "+String1.equalsIgnoreCase(String2));
+	}
+	
 	
 	// public static void main (String[] args) is known as Copy Constructor
 	public static void main(String[] args) {
 		// Calling Default Constructor
 		SampleDemo s = new SampleDemo();
-		new SampleDemo();
+		// new SampleDemo();
 		// Calling Default Constructor
-		SampleDemo s1 = new SampleDemo(5);
-		new SampleDemo(5);
+		// SampleDemo s1 = new SampleDemo(5);
+		// new SampleDemo(5);
 		// s.counter();
 		// s.sCounter();
 		// s.swappingValues();
@@ -107,5 +180,6 @@ public class SampleDemo {
 		// s.add(6);
 		// s.add(3, 5, 7);
 		// s.displayFinal();
+		s.StringDemo();
 	}
 }
