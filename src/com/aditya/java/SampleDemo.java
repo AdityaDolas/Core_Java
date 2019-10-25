1f4124715e901e881f52fe341c46b49ff77b2965
Aditya Dolas <dolasadi@gmail.com>
2019-10-25 15:34:50 +0530

    static

--- src/com/aditya/java/SampleDemo.java
+++ src/com/aditya/java/SampleDemo.java
@@ -3,7 +3,18 @@ package com.aditya.java;
 public class SampleDemo {
 	// instance variable // class variable
 	int i;
-
+	
+	// instance variable // class variable // static variable
+	static int s;
+	
+	// Static Method
+	static void sCounter() {
+		s = 0; // initialized the variable
+		s++;
+		System.out.println(s);
+	}
+	
+	
 	void counter() {
 		// local variable
 		// you have to initialize this variable or it will give compile-time error
@@ -12,9 +23,12 @@ public class SampleDemo {
 		System.out.println(a);
 		System.out.println(a + a);
 	}
-
+	
+	
+	
 	public static void main(String[] args) {
 		SampleDemo s = new SampleDemo();
-		s.counter();
+		// s.counter();
+		s.sCounter();
 	}
 }
