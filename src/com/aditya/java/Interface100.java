ee7663c1a4be85bd1ab82b4e375e0b89d714eb84
Aditya Dolas <dolasadi@gmail.com>
2019-10-29 01:33:56 +0530

    fans Interface

--- src/com/aditya/java/Interface100.java
+++ src/com/aditya/java/Interface100.java
@@ -4,13 +4,27 @@ interface access {
 	void display();
 }
 
-class Interface100 implements access {
+interface fans{
+	void usha();
+	void orient();
+}
+
+class Interface100 implements access, fans {
 	public void display() {
 		System.out.println("example of interface");
+	}
+	
+	public void usha() {
+		System.out.println("usha fans are most populer fan Brand");
+	}
+	public void orient() {
+		System.out.println("Orient brand is considered as one of the most trustable Brand");
 	}
 
 	public static void main(String[] args) {
 		Interface100 a = new Interface100();
 		a.display();
+		a.usha();
+		a.orient();
 	}
 }
