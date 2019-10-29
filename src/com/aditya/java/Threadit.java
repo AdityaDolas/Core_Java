4c5ce2dd0742228644ab42255209b3f8915b7dd7
Aditya Dolas <dolasadi@gmail.com>
2019-10-30 01:59:47 +0530

    sleepy Thread

--- src/com/aditya/java/Threadit.java
+++ src/com/aditya/java/Threadit.java
@@ -8,21 +8,43 @@ public class Threadit extends Thread {
 	public void run() {
 		System.out.println("Thread is running...");
 	}
-	public static void main (String[] argument) {
+
+	public static void main(String[] argument) {
 		Threadit ti = new Threadit();
 		ti.run();
 		ti.start();
 	}
 }
 
-class runit implements Runnable{
+class runit implements Runnable {
 	public void run() {
 		System.out.println("Thread is running... with runnable interface");
 	}
+
 	public static void main(String[] args) {
 		runit i = new runit();
 		Thread t1 = new Thread(i);
 		t1.start();
 	}
-	
+
+}
+
+class sleeping extends Thread {
+	public void run() {
+		for (int i = 0; i <= 100; i++) {
+			try {
+				for (int j = 50; j <= 100; j++) {
+					Thread.sleep(j);
+				}
+			} catch (InterruptedException e) {
+				System.out.println(e);
+			}
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		sleeping sl = new sleeping();
+		sl.start();
+	}
 }
