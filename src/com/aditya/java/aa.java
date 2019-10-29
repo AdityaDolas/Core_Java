b2fb46920a42457e6eaef829fc53d8a86f265719
Aditya Dolas <dolasadi@gmail.com>
2019-10-30 01:35:01 +0530

    constroctor abstraction interface static and final

--- src/com/aditya/java/aa.java
+++ src/com/aditya/java/aa.java
@@ -1,7 +1,43 @@
 package com.aditya.java;
 
-public class aa {
-    public static void main(String[] args) {
-        System.out.println("aa");
-    }
+abstract class display {
+	abstract void m();
+
+	void n() {
+		System.out.println("abstract method body");
+	}
+}
+
+interface fan {
+	public void usha();
+}
+
+public class aa extends display implements fan {
+	public aa() {
+		System.out.println("aa class constructor");
+	}
+	
+	public aa(int a) {
+		a++;
+		System.out.println(a);
+	}
+	
+	
+
+	public void m() {
+		System.out.println("abstraction");
+	}
+
+	public void usha() {
+		System.out.println("usha fans interface");
+	}
+
+	public static void main(String[] args) {
+		aa a = new aa();
+		aa a1 = new aa(87);
+		a.m();
+		a.usha();
+		a.n();
+
+	}
 }
