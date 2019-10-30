b0615939dd3eea47e2590f5c4f790febdc215009
Aditya Dolas <dolasadi@gmail.com>
2019-10-30 23:18:51 +0530

    aic

---
+++ src/com/aditya/java/aic.java
@@ -0,0 +1,29 @@
+package com.aditya.java;
+
+interface ifc{
+	public void fan1();
+}
+
+abstract class afc{
+	public void fan2() {
+		System.out.println("abstract fan");
+	}
+}
+
+public class aic extends afc implements ifc {
+	public aic() {
+		System.out.println("constructor");
+	}
+	
+	@Override
+	public void fan1() {
+		System.out.println("interface fans");
+		
+	}
+	
+	public static void main(String[] args) {
+		aic a = new aic();
+		a.fan1();
+		a.fan2();
+	}
+}
