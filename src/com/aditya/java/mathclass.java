21f11df6c0c395c67705025bbcf8b850141aca21
Aditya Dolas <dolasadi@gmail.com>
2019-10-30 23:28:23 +0530

    aic

---
+++ src/com/aditya/java/mathclass.java
@@ -0,0 +1,12 @@
+package com.aditya.java;
+
+// Java Math class methods are min(),  max(), avg(), sin(), cos(), tan(), roung(), ceil(), floor(), abs() etc.
+public class mathclass {
+	public static void main(String[] args) {
+		double x = 28;
+		double y = 4;
+		
+		// return the maximum of two numbers
+		System.out.println("Maximum number of x and y is: "+Math.max(x, y));
+	}
+}
