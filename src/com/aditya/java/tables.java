d45cefd7a2f7095656a775700f0e31ecfb373e4a
momin danish <dmomn110@gmail.com>
2019-10-31 16:13:23 +0530

    tables updated

--- src/com/aditya/java/tables.java
+++ src/com/aditya/java/tables.java
@@ -2,13 +2,12 @@ package com.aditya.java;
 
 public class tables {
 	public static void main(String[] args) {
-		for (int i = 2; i <= 10;) {
+		for (int i = 1; i <= 10;) {
 			for (int k = 1; k <= 10; k++) {
-				System.out.println(i+"*" + k + " = " + (i * k));
-				System.out.print("\n");
-			}
+				System.out.print(k+"*" + i + "=" + (i * k)+"\t");
+			} 
+			System.out.println();
 			i++;
 		}
-		 
 	}
 }
