7956a20c70fa649652bfe04e7ad15e88cb9000b5
momin danish <dmomn110@gmail.com>
2019-10-31 15:51:06 +0530

    aic2

--- src/com/aditya/java/aic2.java
+++ src/com/aditya/java/aic2.java
@@ -1,12 +1,26 @@
 package com.aditya.java;
 
-interface fanInerface{
+interface fanInterface{
 	public void usha();
 }
 
 abstract class fanAbstract{
 	abstract void orient();
 }
-public class aic2 {
+public class aic2 extends fanAbstract implements fanInterface {
+	public aic2() {
+		System.out.println("constructor");
+	}
+	public void usha() {
 
+		System.out.println("usha fans");
+	}
+	public void orient() {
+		System.out.println("orient fans");
+	}
+	public static void main(String[] args) {
+		aic2 a = new aic2();
+		a.orient();
+		a.usha();
+	}
 }
