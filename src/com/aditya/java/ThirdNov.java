59b508bc3c0d1130fcef55b6c2458cd1cf73ab45
Aditya Dolas <dolasadi@gmail.com>
2019-11-03 19:51:34 +0530

    Abstraction 2

--- src/com/aditya/java/ThirdNov.java
+++ src/com/aditya/java/ThirdNov.java
@@ -4,12 +4,33 @@ abstract class a1 {
 	abstract void displaya1();
 }
 
+abstract class a2{
+	public a2() {
+		System.out.println("Abstract class constructor");
+	}
+	
+	
+	public void display2(){
+		// why this is not printing
+		System.out.println("Abstraction2");
+	}
+}
+
 public class ThirdNov extends a1 {
 	public void displaya1() {
 		System.out.println("Abstraction");
 	}
+	public void display2() {
+		System.out.println("Abstraction3");
+	}
+	
 	public static void main(String[] args) {
 		ThirdNov tn = new ThirdNov();
+		a2 a = new a2() {
+			// What to write here
+			// System.out.println("hi");
+		};
 		tn.displaya1();
+		tn.display2();
 	}
 }
