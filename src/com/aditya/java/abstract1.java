27f90548e65bb774be3c53f0497cd4fd1c5813ca
Aditya Dolas <dolasadi@gmail.com>
2019-11-03 15:54:47 +0530

    codechef update 2

--- src/com/aditya/java/abstract1.java
+++ src/com/aditya/java/abstract1.java
@@ -3,16 +3,16 @@ package com.aditya.java;
 
 // If we do not give body to the method inside abstract class it will give compilation error.
 // To you want to remove the error you have to add abstract keyword to the method.
-abstract class dog{
+abstract class dog123{
 	abstract void run();
 }
 
-public class abstract1 extends dog {
+public class abstract1 extends dog123 {
 	void run() {
 		System.out.println("Running safly");
 	}
 	public static void main(String[] args) {
-		dog d = new abstract1();
+		dog123 d = new abstract1();
 		d.run();
 	}
 }
--- src/com/aditya/java/codechef.java
+++ src/com/aditya/java/codechef.java
@@ -3,7 +3,7 @@ package com.aditya.java;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class codechef {
+/*public class codechef {
 	public static void main(String[] args) {
 		ArrayList<String> weapons = new ArrayList<>(10);
 
@@ -19,7 +19,7 @@ public class codechef {
 			// for(int j=0; j<2; j++) {
 			// System.out.println(weapons.get(0).charAt(0));
 
-			if (weapons.get(0).charAt(0) == 1 || weapons.get(1).charAt(1) == 1) {
+			if (weapons.get(0).charAt(0) == 1 || weapons.get(1).charAt(0) == 1) {
 				System.out.println("1");
 			} else {
 				System.out.println("0");
@@ -27,8 +27,41 @@ public class codechef {
 
 		}
 
-		//System.out.println(weapons);
+		// System.out.println(weapons);
 
 		testCases--;
+	}
+}
+*/
+public class codechef {
+	public static void main(String[] args) {
+		ArrayList<String> weapons = new ArrayList<>();
+
+		// First input
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		while (t > 0) {
+			int n = sc.nextInt();
+			for (int i = 0; i < n; i++) {
+				String xor = "";
+				for (int j = 0; j < 10; j++) {
+					if (weapons.get(i).charAt(j) == weapons.get(i + 1).charAt(j)) {
+						xor += '0';
+
+					} else {
+						xor += '1';
+					}
+				}
+				weapons.set(i+1, xor);
+			}
+			int result =0 ;
+			for (int i=0;i<10;i++) {
+				if (weapons.get(n-1).charAt(i)=='1') {
+					result++;
+				}
+			}
+			
+		}
 	}
 }
