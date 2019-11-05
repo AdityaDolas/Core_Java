c99e4a4cb6f62b249db4be3c38ad30a1bfc88a51
Aditya Dolas <dolasadi@gmail.com>
2019-11-05 23:57:22 +0530

    aic 5/11

---
+++ src/com/aditya/java/aicp.java
@@ -0,0 +1,26 @@
+package com.aditya.java;
+
+interface inter1{
+	public void displayinter1();
+}
+
+abstract class abs1{
+	public abstract void displayabs1();
+}
+public class aicp extends abs1 implements inter1 {
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void displayinter1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayabs1() {
+		// TODO Auto-generated method stub
+		
+	}
+}
