3058b75fc0dd839341d6826c66ad3fe956fbc1b3
RupaliGhag <rupalighag1997@gmail.com>
2019-11-07 16:13:47 +0530

    extending thread

--- src/com/aditya/java/MainThreadDemo.java
+++ src/com/aditya/java/MainThreadDemo.java
@@ -18,3 +18,90 @@ public class MainThreadDemo {
 	}
 	
 }
+
+// How to run Tread by extending
+class Multi extends Thread{
+	public void run() {
+		System.out.println("Thread is running...");
+	}
+	public static void main(String[] args) {
+		Multi m = new Multi();
+		m.start();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------
