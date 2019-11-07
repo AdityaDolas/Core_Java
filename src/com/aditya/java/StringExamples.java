3ddf65bc0b03c1abc0860eabf1be82364c395743
RupaliGhag <rupalighag1997@gmail.com>
2019-11-07 18:58:38 +0530

    String666

--- src/com/aditya/java/StringExamples.java
+++ src/com/aditya/java/StringExamples.java
@@ -61,18 +61,71 @@ class reverseStringScanner {
 		// Converting String to Character array
 		char[] b = g.toCharArray();
 		g = "";
-
+		
+		// iterating the array
 		for (int i = b.length - 1; i >= 0; i--) {
 			g = g + b[i];
 		}
 		System.out.println(g);
 
 	}
-
 }
 
+// Codes Technology = ygolonhceT sedoC
+class reverseStringandchar{
+	public static void main(String[] args) {
+		String a = "aditya dolas";
+		System.out.println(a+" = ");
+		String[] b = a.split("");
+		a="";
+		
+		for(int i = b.length-1; i>=0 ; i--) {
+			a =  a + b[i];
+		}
+		System.out.println(a);
+	}
+}
 
+// reverseStringandchar Scanner
+class reverseStringandcharScanner{
+	public static void main(String[] args) {
+		System.out.println("Enter:");
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		
+		String[] b = a.split("");
+		a="";
+		
+		for(int i = b.length-1; i>=0; i--) {
+			a = a + b[i];
+		}
+		System.out.println(a);
+	}
+}
 
+// Codes Technology = sedoC ygolonhceT 
+class String5{
+	public static void main(String[] args) {
+		System.out.println("Enter: ");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+		System.out.println(s+" = ");
+		String[] sa = s.split(" ");
+		
+		s = "";
+		
+		for(int i = 0 ;i<sa.length; i++) {
+			String[] temp = sa[i].split("");
+			for(int j = temp.length-1; j>=0; j--) {
+				s=s+temp[j];
+			}
+			s=s+"";
+		}
+		
+		System.out.println(s);
+	}
+}
 
 
 
