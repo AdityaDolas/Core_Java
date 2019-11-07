a12d5e47abd2e7c42f2c047c431546b36740b3bd
RupaliGhag <rupalighag1997@gmail.com>
2019-11-07 17:21:48 +0530

    Array char to String

--- src/com/aditya/java/MainThreadDemo.java
+++ src/com/aditya/java/MainThreadDemo.java
@@ -68,6 +68,35 @@ class TestThreadClass extends Thread{
 	public static void main(String[] args) {
 		TestThreadClass  tc = new TestThreadClass();
 		tc.start();
+		tc.start();
+		
+	}
+}
+
+class TestJoinMethod1 extends Thread {
+	public void run() {
+		for(int i =1; i<5;i++) {
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		TestJoinMethod1 t1 = new TestJoinMethod1();
+		TestJoinMethod1 t2 = new TestJoinMethod1();
+		TestJoinMethod1 t3 = new TestJoinMethod1();
+		t1.start();
+		try {
+			t1.join();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		t2.start();
+		t3.start();
+
 	}
 }
 
---
+++ src/com/aditya/java/StringExamples.java
@@ -0,0 +1,13 @@
+package com.aditya.java;
+
+// Array char to String
+
+public class StringExamples {
+	public static void main(String[] args) {
+		char[] cs = {'A','D','I','T','Y','A'};
+		String b = "";
+		for(char c: cs) {
+			System.out.println(c);
+		}
+	}
+}
