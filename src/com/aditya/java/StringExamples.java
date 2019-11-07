42470cbb35d421b1cd2cc5895e4981b8d215025a
RupaliGhag <rupalighag1997@gmail.com>
2019-11-07 18:05:09 +0530

    Reverse String Scanner

--- src/com/aditya/java/StringExamples.java
+++ src/com/aditya/java/StringExamples.java
@@ -7,31 +7,78 @@ import java.util.Scanner;
 
 public class StringExamples {
 	public static void main(String[] args) {
-		char[] cs = {'A','D','I','T','Y','A'};
+		char[] cs = { 'A', 'D', 'I', 'T', 'Y', 'A' };
 		String b = "";
-		for(char c: cs) {
+		for (char c : cs) {
 			System.out.println(c);
 		}
 	}
 }
 
 // Array char to String using scanner
+class StringExamples1 {
+	public static void main(String[] args) {
+		// Taking input
+		System.out.println("Enter: ");
+		Scanner sc = new Scanner(System.in);
+		String g = sc.next();
 
-class StringExamples1{
+		// Converting String to array
+		char[] cArray = g.toCharArray();
+		String b = "";
+
+		// iterating the character
+		for (char c : cArray) {
+			System.out.println(c);
+		}
+
+	}
+}
+
+// Reverse String
+class reverseString {
+	public static void main(String[] args) {
+		
+		String a = "Niit";
+		char[] b = a.toCharArray();
+
+		a = "";
+		for (int i = b.length - 1; i >= 0; i--) {
+			a = a + b[i];
+		}
+		System.out.println(a);
+	}
+}
+
+// Reverse String Scanner
+class reverseStringScanner {
 	public static void main(String[] args) {
 		// Taking input
 		System.out.println("Enter: ");
 		Scanner sc = new Scanner(System.in);
 		String g = sc.next();
 		
-		// Converting String to array
-		char [] cArray = g.toCharArray();
-		String b = "";
-		
-		// iterating the character
-		for(char c : cArray) {
-			System.out.println(c);
+		// Converting String to Character array
+		char[] b = g.toCharArray();
+		g = "";
+
+		for (int i = b.length - 1; i >= 0; i--) {
+			g = g + b[i];
 		}
-		
+		System.out.println(g);
+
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------
