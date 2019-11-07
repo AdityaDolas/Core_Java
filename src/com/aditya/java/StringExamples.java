5a99d42af19f61d28a4669861b391185ce5186f3
RupaliGhag <rupalighag1997@gmail.com>
2019-11-07 17:36:58 +0530

    Array char to String using scanner

--- src/com/aditya/java/StringExamples.java
+++ src/com/aditya/java/StringExamples.java
@@ -1,5 +1,8 @@
 package com.aditya.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Array char to String
 
 public class StringExamples {
@@ -9,5 +12,26 @@ public class StringExamples {
 		for(char c: cs) {
 			System.out.println(c);
 		}
+	}
+}
+
+// Array char to String using scanner
+
+class StringExamples1{
+	public static void main(String[] args) {
+		// Taking input
+		System.out.println("Enter: ");
+		Scanner sc = new Scanner(System.in);
+		String g = sc.next();
+		
+		// Converting String to array
+		char [] cArray = g.toCharArray();
+		String b = "";
+		
+		// iterating the character
+		for(char c : cArray) {
+			System.out.println(c);
+		}
+		
 	}
 }
