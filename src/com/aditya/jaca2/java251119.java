3791cc8b73e7609f875257ae80e93238562c4695
Aditya Dolas <dolasadi@gmail.com>
2019-11-25 18:35:28 +0530

    C:/Users/Aditya/git/Core_Java

---
+++ src/com/aditya/jaca2/java251119.java
@@ -0,0 +1,17 @@
+package com.aditya.jaca2;
+
+public class java251119 {
+	public static void main(String[] args) {
+		System.out.println("Java 25/11/19 practice");
+	}
+}
+
+class areaOfCircle1 {
+	public static void main(String[] args) {
+		double rad;
+		final double PI = 3.14159;
+		rad = 100;
+		double area = PI * rad * rad;
+		System.out.println("Area of circle is = " + area);
+	}
+}
