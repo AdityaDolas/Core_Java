a2c9d06b804bfc2c63df788df86cde4fb7e4767f
Aditya Dolas <dolasadi@gmail.com>
2019-12-01 23:19:05 +0530

    left pattern

--- src/com/aditya/jaca2/java251119.java
+++ src/com/aditya/jaca2/java251119.java
@@ -273,3 +273,14 @@ class LucasTest {
 
 	}
 }
+
+class left{
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j =0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
