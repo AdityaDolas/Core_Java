628dd77d8826e558b335896dac8ed0e2f171679c
Aditya Dolas <dolasadi@gmail.com>
2019-12-01 23:27:56 +0530

    right pattern

--- src/com/aditya/jaca2/java251119.java
+++ src/com/aditya/jaca2/java251119.java
@@ -274,11 +274,22 @@ class LucasTest {
 	}
 }
 
-class left{
+class leftPattern {
 	public static void main(String[] args) {
-		for(int i=0;i<5;i++) {
-			for(int j =0;j<=i;j++) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+class rightPattern {
+	public static void main(String[] args) {
+		for (int i = 5; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(" *");
 			}
 			System.out.println();
 		}
