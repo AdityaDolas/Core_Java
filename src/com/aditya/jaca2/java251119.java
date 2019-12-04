2f67e58b38bf808ff98e0de0d84bba03c284245e
Aditya Dolas <dolasadi@gmail.com>
2019-12-05 00:30:47 +0530

    reverse sorting array

--- src/com/aditya/jaca2/java251119.java
+++ src/com/aditya/jaca2/java251119.java
@@ -392,15 +392,19 @@ class arraySort {
 
 		for (int i = 0; i < a.length - 1; i++) {
 			for (int j = 0; j < (a.length - 1 - i); j++) {
-				if (a[j] > a[j+1]) {
+				if (a[j] > a[j + 1]) {
 					int t = a[j];
 					a[j] = a[j + 1];
 					a[j + 1] = t;
 				}
 			}
 		}
-		System.out.println("Sorted Array:");
+		System.out.println("\nSorted Array:");
 		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("\nReverse Array:");
+		for (int i = a.length-1; i >=0; i--) {
 			System.out.print(a[i] + " ");
 		}
 	}
