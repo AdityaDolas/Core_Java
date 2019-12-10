f0dbce41f4c14207074cd303775acf0f8fa3da82
Aditya Dolas <dolasadi@gmail.com>
2019-12-10 15:44:50 +0530

    is 93 prime

--- src/com/aditya/jaca2/trytocatch.java
+++ src/com/aditya/jaca2/trytocatch.java
@@ -11,3 +11,14 @@ public class trytocatch {
 		}
 	}
 }
+
+class prime93{
+	public static void main(String[] args) {
+		int a=93;
+		for(int i=1; i<=93; i++) {
+			if(93%i==0){
+				System.out.println(i);
+			}
+		}
+	}
+}
