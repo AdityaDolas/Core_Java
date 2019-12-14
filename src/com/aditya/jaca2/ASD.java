d0c01456e7eb31cbd3e9c3d005b9a9e0694b8bde
Aditya Dolas <dolasadi@gmail.com>
2019-12-14 16:25:38 +0530

    hashmapping

--- src/com/aditya/jaca2/ASD.java
+++ src/com/aditya/jaca2/ASD.java
@@ -3,5 +3,6 @@ package com.aditya.jaca2;
 public class ASD {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		
 	}
 }
---
+++ src/com/aditya/jaca2/ioCollection.java
@@ -0,0 +1,10 @@
+package com.aditya.jaca2;
+
+import java.util.HashMap;
+
+public class ioCollection {
+	public static void main(String[] args) {
+		HashMap<String, Integer> hMap=new HashMap<>();
+		System.out.println(hMap);
+	}
+}
